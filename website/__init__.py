9fa57efdd6ea22afb36f9b83f7bbed88964f143e
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-18 20:30:01 +0200

    sign up routing from views

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dhdododpw'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     #import our  views routing and authetication
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,38 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template,request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
     return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
     return "<p>logout</p>"
 
-@auth.route("/signup")
+@auth.route("/signup", methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(name) < 2:
+            flash("Name must be greater than 1 character", category='error')
+        elif password1 != password2:
+            flash("password must match to be confirmed", category="error")
+        elif (len(password1) < 7):
+            flash("password must be atleast 7 characters", category='error')
+        
+        else:
+            flash("Account created", category="success")
     return render_template("sign_up.html")
